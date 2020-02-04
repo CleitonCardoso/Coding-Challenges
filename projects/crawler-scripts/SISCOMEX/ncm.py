481b6f4b06699c1b93a68c219b65e3a3ba578cdf
Cleiton Cardoso <cleitoncardoso.dev@outlook.com>
2020-02-04 19:41:26 -0300

    added new script

---
+++ projects/crawler-scripts/SISCOMEX/ncm.py
@@ -0,0 +1,20 @@
+import requests
+
+text_file = open("output.csv", "w")
+
+def request(index):
+    headers = {'X-CSRF-Token': 'M0nEAWcFFGkT/So8fMWJXKyHUCCB0c0RIxZxdNNww4F09k9xdqgUE6S7Sy1lag2Z11QtaeKq1fI='}
+    data = {
+    }
+    page = ('0' + str(index), str(index))[index >= 10]
+    url = 'https://portalunico.siscomex.gov.br/classif/api/nomenclatura/'+ page + '?dataInteresse=2020-01-01T23:00:00.000Z'
+    response = requests.get(url , headers= headers , data = data, verify=False)
+    if (response.status_code == 200):
+        json = response.json()
+        for filho in json['filhos']:
+            text_file.write(filho['codigo'] + ';' + filho['nomeExtenso'] + '\n')
+
+for index in range(1,98):
+    request(index)
+
+text_file.close()
