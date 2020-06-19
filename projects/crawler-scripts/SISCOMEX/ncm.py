db0568812bea641edd7187ca2aad309ed2d8051e
Cleiton Cardoso <cleitoncardoso.dev@outlook.com>
2020-06-19 14:16:02 -0300

    Update ncm.py

--- projects/crawler-scripts/SISCOMEX/ncm.py
+++ projects/crawler-scripts/SISCOMEX/ncm.py
@@ -3,12 +3,13 @@ import requests
 text_file = open("output.csv", "w")
 
 def request(index):
-    headers = {'X-CSRF-Token': 'M0nEAWcFFGkT/So8fMWJXKyHUCCB0c0RIxZxdNNww4F09k9xdqgUE6S7Sy1lag2Z11QtaeKq1fI='}
+    headers = {'X-CSRF-Token': 'Xsy80i/1iL0suSNpZAnsM4RvuNw6VPAzphdgVQqK6efgQo5xWWkJ/RgywAfxGlcmbJA4zsc53lI='}
     data = {
     }
     page = ('0' + str(index), str(index))[index >= 10]
-    url = 'https://portalunico.siscomex.gov.br/classif/api/sumario/'+ page + '?dataInteresse=2020-01-01T23:00:00.000Z'
+    url = 'https://portalunico.siscomex.gov.br/classif/api/nomenclatura/'+ page + '?dataInteresse=2020-06-19T16:44:50.000Z'
     response = requests.get(url , headers= headers , data = data, verify=False)
+    print(response.status_code)
     if (response.status_code == 200):
         json = response.json()
         for filho in json['filhos']:
